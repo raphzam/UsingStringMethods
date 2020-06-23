2129c790a553ae5876e909941d591d34d4b22e18
raphzam <raphaeljasonzamora@gmail.com>
2020-06-23 09:42:52 -0400

    added index is larger than length option

--- src/UsingStringMethods.java
+++ src/UsingStringMethods.java
@@ -11,6 +11,10 @@ public class UsingStringMethods {
 
         System.out.println("Enter a number as an index: ");
         int userIndex = input.nextInt();
+        if (userIndex > userWord.length()){
+            System.out.println("Index is larger than length.");
+            return;
+        }
 
         System.out.printf("The length of %s is %d\n", userWord, userIndex);
         System.out.printf("The substring from 0 to %d is " + userWord.substring(0, userIndex), userIndex);
