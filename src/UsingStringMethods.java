2b7d3f86389a748afaf1ba8b4d2cb5e1a7cc0c58
raphzam <raphaeljasonzamora@gmail.com>
2020-06-23 09:39:20 -0400

    initial commit

---
+++ src/UsingStringMethods.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class UsingStringMethods {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Enter a word:");
+        String userWord = input.nextLine();
+
+        System.out.println("Enter a number as an index: ");
+        int userIndex = input.nextInt();
+
+        System.out.printf("The length of %s is %d\n", userWord, userIndex);
+        System.out.printf("The substring from 0 to %d is " + userWord.substring(0, userIndex), userIndex);
+    }
+
+}
